8914c7ae1f5d42a0e69a687c1efaebbbf2809058
SaydsC <G00411297@gmit.ie>
2023-12-11 08:55:31 +0000

    flask

--- flaskserver.py
+++ flaskserver.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:S@die!11!@localhost/db_name'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:*****@localhost/db_name'
 #Reminder Replace username, password, local host and db_name with my SQL credentials
 
 db= SQLAlchemy(app)
@@ -27,7 +27,7 @@ def get_users():
         user_list.append({'id':user.id, 'username':user.username})
     return jsonify({'users':user_list})
 
-if __name__=='__main__':
+if __name__=="__main__":
     app.run(debug=True)
 
  #This connects Flask to MySQL database
