f3eb9ead5aa84f64b65f431a19ea1af0c8ab14ca
SaydsC <G00411297@gmit.ie>
2023-12-18 21:50:30 +0000

    rest server

--- rest_server.py
+++ rest_server.py
@@ -28,29 +28,53 @@ def findByID(id):
     return jsonify(foundBooks[0])
 
 #Create
+# curl -X POST -H "content-type:application/json" -d "{\"Title\":\"test\", \"Author\":\"Mr Test\", \"Price\":123}" http://127.0.0.1:5000/books
+
 @app.route('/books', methods=['POST'])
 def create():
     global nextId
+    if not request.json:
+        abort(400)
 
     book = {
         "id":nextId,
-        "Title": "XXX",
-        "Author": "XX",
-        "Price": 111
+        "Title": request.json["Title"],
+        "Author": request.json["Author"],
+        "Price": request.json["Price"]
     }
     books.append(book)
     nextId += 1
     return jsonify(book)
 
 #Update
+# curl -X PUT -d "{\"Title\":\"New Title\", \"Price\":999}" -H "content-type:application/json" http://127.0.0.1:5000/books/1
+
 @app.route('/books/<int:id>', methods=['PUT'])
 def update(id):
-    return "served by update with id"+ str(id)
+    foundBooks = list(filter (lambda t : t["id"]== id, books))
+    if len(foundBooks) == 0:
+        return jsonify({}), 404
+    currentBook = foundBooks[0]
+    if 'Title' in request.json:
+        currentBook['Title'] = request.json['Title']
+    if 'Author' in request.json:
+        currentBook['Author'] = request.json['Author']
+    if 'Price' in request.json:
+        currentBook['Price'] = request.json['Price']
+    
+    return jsonify(currentBook)
 
 #Delete
+# curl -X DELETE http://127.0.0.1:5000/books/1
+
 @app.route('/books/<int:id>', methods=['DELETE'])
 def delete(id):
-    return "served by delete with id"+ str(id)
+    foundBooks = list(filter (lambda t : t["id"]== id, books))
+    if len(foundBooks) == 0:
+        return jsonify({}), 404
+    books.remove(foundBooks[0])
+
+    return jsonify({"done":True})
 
 if __name__=="__main__":
     app.run(debug=True)
