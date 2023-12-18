5ea6c396e61ce66655ce72e7610d7ee6bd20acf0
SaydsC <G00411297@gmit.ie>
2023-12-18 15:21:02 +0000

    server

--- rest_server.py
+++ rest_server.py
@@ -1,6 +1,14 @@
-from flask import Flask, url_for, request, redirect, abort
+from flask import Flask, jsonify, url_for, request, redirect, abort
 
 app=Flask(__name__, static_url_path='', static_folder='staticpages')
+
+books=[
+    {"id": 1, "Title": "Harry Potter", "Author": "JK", "Price": 1000}, 
+    {"id": 2, "Title": "Cook Book", "Author": "SC", "Price": 900}, 
+    {"id": 3, "Title": "Other Book", "Author": "LM", "Price": 1100}
+]
+
+nextID=4
 
 @app.route('/')
 def index():
@@ -9,12 +17,15 @@ def index():
 #get all
 @app.route('/books')
 def getAll():
-    return "served by GET all()"
+    return jsonify(books)
 
 #find by ID
 @app.route('/books/<int:id>')
 def findByID(id):
-    return "served by find by ID with id"+ str(id)
+    foundBooks = list(filter (lambda t : t["id"]== id, books))
+    if len(foundBooks == 0):
+        return jsonify({}), 204
+    return jsonify(foundBooks[0])
 
 #Create
 @app.route('/books')
